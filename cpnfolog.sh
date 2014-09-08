a6e58788f78acbde49f867438913e6397ae22050
fissionchris <fissionchris@yahoo.com>
2014-09-08 04:13:25 -0500

    Create cpnfolog.sh

---
+++ cpnfolog.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+
+###-------------------------------------------------------###
+  #------------:  Part 1: Remotely Connect  :-------------#
+  # 				Using Central Host   				  #
+###-------------------------------------------------------###
+# 	This could use any central hub as a host
+
+-------------------------------------------------------------
+#############################################################
+-------------------------------------------------------------
+
+###-------------------------------------------------------###
+  #------------:  Part 2: Gather All Data  :--------------#
+  # 				Write to Std Format   				  #
+###-------------------------------------------------------###
+# 	Gathers versions for relevant programs & libraries
+
+cd /root
+
+# nfolog="cpnfolog_$(date +%y%m%d%H%M).log"
+nfolog="cpnfolog_$(date +%y%m%d).log"
+
+#}------: timestamp :--------------------------------------->
+# time/date stamp code here
+
+#}------: disk uage  :-------------------------------------->
+###-------------------------------------------------------###
+#tmpStr=`df -h | grep home | awk {'print $6,$2,$3,$4,$5'}`
+#echo "diskuse:$tmpStr" >> $nfolog
+###-------------------------------------------------------###
+
+
+#}------: hostname :---------------------------------------->
+tmpStr=`uname -a | awk {'print $2'}`
+echo "hostname:$tmpStr" >> $nfolog
+
+#}------: os :---------------------------------------------->
+tmpStr=`cat /etc/redhat-release | awk {'print $4'}`
+echo "OS:$tmpStr" >> $nfolog
+
+#}------: kernel :------------------------------------------>
+tmpStr=`uname -a | awk {'print $3'}`
+echo "kernel:$tmpStr" >> $nfolog
+
+#}------: apache :------------------------------------------>
+tmpStr=`httpd -v | grep Apache\/ | awk {'print $3'}`
+echo "apache:$tmpStr" >> $nfolog
+tmpStr=`httpd -v | grep Cpanel | awk {'print $2'}`
+echo "EasyApache:$tmpStr" >> $nfolog
+
+#}------: perl :-------------------------------------------->
+tmpStr=`perl -v | grep "v[[:digit:]]" | awk {'print $4'}`
+echo "perl:$tmpStr" >> $nfolog
+
+#}------: php :--------------------------------------------->
+tmpStr=`php -v | grep cli | awk {'print $2'}`
+echo "PHP:$tmpStr" >> $nfolog
+tmpStr=`php -v | grep Engine | awk {'print $3'}`
+echo "ZendEngine:$tmpStr" >> $nfolog
+tmpStr=`php -v | grep eAccelerator | awk {'print $3'}`
+echo "eAccelerator:$tmpStr" >> $nfolog
+tmpStr=`php -v | grep ionCube | awk {'print $6'}`
+echo "ionCube:$tmpStr" >> $nfolog
+tmpStr=`php -v | grep Guard | awk {'print $5'}`
+echo "ZendGuard:$tmpStr" >> $nfolog
+tmpStr=`php -v | grep Suhosin | awk {'print $3'}`
+echo "Suhosin:$tmpStr" >> $nfolog
+
+#}------: mysql :------------------------------------------->
+tmpStr=`mysql -V | awk {'print $5'}`
+echo "mysql:$tmpStr" >> $nfolog
+
+#}------: cpanel :------------------------------------------>
+tmpStr=`/usr/local/cpanel/cpanel -V`
+echo "cpanel:$tmpStr" >> $nfolog
+
+
+
+###-------------------------------------------------------###
+  #------------:  Part 3: Capture Data  :-----------------#
+  # 			   Merge w/ Historical   				  #
+###-------------------------------------------------------###
+
+#}------: transmit :---------------------------------------->
+# code to send results log to an email, scp to central 
+# server, and/or write results to db
